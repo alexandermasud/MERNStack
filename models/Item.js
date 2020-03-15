e1d355ce973bb5f62ed597255a73703c7ba2fde5
Alexander Masud <alexandermasud@gmail.com>
2020-03-15 14:04:09 +0100

    Server completed

---
+++ models/Item.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Created Schema
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: false
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Item = mongoose.model("item", ItemSchema);
