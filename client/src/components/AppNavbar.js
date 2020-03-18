38b5cb0ca0a097a80ff8386e968c42c997031a24
Alexander Masud <alexandermasud@gmail.com>
2020-03-18 17:57:06 +0100

    Update AppNavbar.js

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -31,7 +31,7 @@ class AppNavbar extends Component {
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
-                  <NavLink href="https://github.com/alexandermasud">
+                  <NavLink href="https://github.com/alexandermasud/MERNStack">
                     Github
                   </NavLink>
                 </NavItem>
